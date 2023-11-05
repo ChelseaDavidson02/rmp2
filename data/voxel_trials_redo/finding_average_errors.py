627717fcb0d39f2ec05202cbf810d24cf8e6cd6b
ChelseaDavidson02 <davidson.chelsea@gmail.com>
2023-11-05 23:59:46 +1000

    Uploaded data from oblation study, random env images, and voxel trial redos.

---
+++ data/voxel_trials_redo/finding_average_errors.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+# Load the data from the CSV file
+voxel_size = ['001', '020', '040', '060', '080', '100', '120', '140', '160']
+trials = ['a', 'b', 'c']
+dst = 0.2
+
+percentage_errors = {}
+for voxl in voxel_size:
+    voxl_percentage_errors = []
+    for trial in trials:
+        path = f"data/voxel_trials_redo/error_data_File_0.{voxl}{trial}.csv"
+        try:
+            loaded_data = np.loadtxt(path, delimiter=",", skiprows=1)
+        except:
+            break
+
+        # The 'delimiter' argument should match the delimiter used in the CSV file (a comma in this case).
+        # The 'skiprows' argument is set to 1 to skip the header row.
+
+        # Now, you can access the loaded x and y values as separate NumPy arrays
+        y_dst_loaded = loaded_data[:, 0]  # First column (x-values)
+        error_loaded = loaded_data[:, 1]  # Second column (y-values)
+
+        first = True
+        final_error_loaded = []
+        final_y_dst_loaded = []
+        for i in range(len(error_loaded)):
+            if error_loaded[i] < 0.01 and first:
+                first = False
+            if not first:
+                final_error_loaded.append(error_loaded[i])
+                final_y_dst_loaded.append(y_dst_loaded[i])
+
+        final_error_loaded = np.array(final_error_loaded)
+        percent_array = np.full(final_error_loaded.shape, 100/dst)
+        percentage_error_values =  final_error_loaded * percent_array
+        
+        average_error = np.average(final_error_loaded)
+        average_percentage_error = (average_error / dst) *100
+        
+        voxl_percentage_errors.append(average_percentage_error)
+
+    average_voxel_p_error =  np.average(voxl_percentage_errors)
+    percentage_errors[voxl] = average_voxel_p_error
+
+errors = []
+for key in percentage_errors.keys():
+    errors.append(percentage_errors[key])
+print(f"percentage_errors = {errors};")
---
+++ data/voxel_trials_redo/finding_graph_building_times.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+# Load the data from the CSV file
+voxel_size = ['001', '020', '040', '060', '080', '100', '120', '140', '160']
+trials = ['a', 'b', 'c']
+dst = 0.2
+
+voxel_times = {}
+for voxl in voxel_size:
+    voxel_map = {}
+    voxel_map['forward_pass'] = []
+    voxel_map['rmp_evaluation'] = []
+    voxel_map['backward_pass'] = []
+    voxel_map['resolve'] = []
+
+    voxel_times[voxl] = voxel_map
+
+path = f"data/voxel_data_trials/rmp_timing.txt"
+voxel_map = {}
+
+with open(path, 'r') as file:
+    # Skip the header line
+    next(file)
+    
+    for line in file:
+        # Split the line into columns based on the comma delimiter
+        columns = line.strip().split(',')
+
+        name = columns[0]
+        vox_sz = name[5:8]
+        # print(vox_sz)
+
+        voxel_map = voxel_times[vox_sz]
+        voxel_map['forward_pass'].append(float(columns[1]))
+        voxel_map['rmp_evaluation'].append(float(columns[2]))
+        voxel_map['backward_pass'].append(float(columns[3]))
+        voxel_map['resolve'].append(float(columns[4]))
+
+
+
+graph_building_time_map = {}
+for key in voxel_times.keys():
+    voxel_map = voxel_times[key]
+    new_voxel_map = {}
+    times = []
+    new_voxel_map['ave_forward_pass'] = np.average(np.array(voxel_map['forward_pass']))
+    times.append(new_voxel_map['ave_forward_pass'])
+    new_voxel_map['ave_rmp_evaluation'] = np.average(np.array(voxel_map['rmp_evaluation']))
+    times.append(new_voxel_map['ave_rmp_evaluation'])
+    new_voxel_map['ave_backward_pass'] = np.average(np.array(voxel_map['backward_pass']))
+    times.append(new_voxel_map['ave_backward_pass'])
+    new_voxel_map['ave_resolve'] = np.average(np.array(voxel_map['resolve']))
+    times.append(new_voxel_map['ave_resolve'])
+    new_voxel_map['total_time'] = np.sum(np.array(times))
+
+    graph_building_time_map[key] = new_voxel_map
+
+
+ave_forward_passes = []
+ave_rmp_evaluations = []
+ave_backward_passes = []
+ave_resolves = []
+total_times = []
+
+for key in graph_building_time_map.keys():
+    ave_forward_passes.append(graph_building_time_map[key]['ave_forward_pass'])
+    ave_rmp_evaluations.append(graph_building_time_map[key]['ave_rmp_evaluation'])
+    ave_backward_passes.append(graph_building_time_map[key]['ave_backward_pass'])
+    ave_resolves.append(graph_building_time_map[key]['ave_resolve'])
+    total_times.append(graph_building_time_map[key]['total_time'])
+    # print(f"{key}: {voxel_times[key]}\n")
+# print(voxel_times)
+
+print(f"ave_forward_passes = {ave_forward_passes};\n")
+print(f"ave_rmp_evaluations = {ave_rmp_evaluations};\n")
+print(f"ave_backward_passes = {ave_backward_passes};\n")
+print(f"ave_resolves = {ave_resolves};\n")
+print(f"total_times = {total_times};\n")
